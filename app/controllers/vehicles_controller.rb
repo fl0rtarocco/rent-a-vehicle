50097f448677d78a4dca83bdd45bb8ef27dbdf59
Anton Brinckmann <antonbrinckmann@icloud.com>
2022-01-29 14:25:57 +0100

    create-routes

---
+++ app/controllers/vehicles_controller.rb
@@ -0,0 +1,25 @@
+class VehiclesController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
