a75347416085795a5668e0c5343fb1fbc30484de
Florencia Tarocco <florenciatarocco@gmail.com>
2022-01-29 18:33:34 +0100

    create shitty homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_162250) do
+ActiveRecord::Schema.define(version: 2022_01_29_170319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2022_01_29_162250) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
     t.float "price"
+    t.string "name"
     t.index ["user_id"], name: "index_vehicles_on_user_id"
   end
 
