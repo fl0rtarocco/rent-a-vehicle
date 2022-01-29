3bcfbd8664bbb3994dee6977354a2e1f0ef798e7
peder frang <peder.frang@gmail.com>
2022-01-29 17:28:44 +0100

    Added destroy dependancy for user and vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,5 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :category, presence: true, inclusion: { in: ["Elevator", "Car", "Motorcycle", "Scooter", "Giraffe", "Truck", "Tanks", "Private Jet"] }
 end
