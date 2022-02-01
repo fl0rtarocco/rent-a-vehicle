b234b40edacdb179de68cab8a361d19b60452160
Anton Brinckmann <antonbrinckmann@icloud.com>
2022-02-01 19:38:52 +0100

    edit-vehicle-fix

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -6,4 +6,17 @@ class VehiclesController < ApplicationController
   def show
     @vehicle = Vehicle.find(params[:id])
   end
+  def edit
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def update
+    @vehicle = Vehicle.find(params[:id])
+    @vehicle.update(vehicle_params)
+    redirect_to vehicle_path(@vehicle)
+  end
+  def vehicle_params
+    params.require(:vehicle).permit(:category, :price, :description, :name)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   get 'users/update'
   devise_for :users
   root to: 'pages#home'
-  get '/vehicles/:id/edit', to:"users#edit"
-  patch '/vehicles/:id', to:"users#update"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :vehicles, only: [ :index, :show ]
+  resources :vehicles, only: [ :index, :show, :edit, :update ]
 end
