da48f98427d25fb975d3beb18dbad94b939b312d
Anton Brinckmann <antonbrinckmann@icloud.com>
2022-02-01 17:50:13 +0100

    edit-vehicle

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/update'
   devise_for :users
   root to: 'pages#home'
+  get '/vehicles/:id/edit', to:"users#edit"
+  patch '/vehicles/:id', to:"users#update"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :vehicles, only: [ :index, :show ]
 end
