e15225cba00c23495b86be8f836f969cd23131ea
peder frang <peder.frang@gmail.com>
2022-02-03 19:32:21 +0100

    fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     else
       @vehicles = Vehicle.where(user_id: current_user.id)
     end
+    authorize @vehicles
     @name = "#{@vehicles.first.user.first_name} #{@vehicles.first.user.last_name}"
   end
 end
