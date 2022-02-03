1617ec8c831bee03e61a61bcd443f1aa35549bc3
peder frang <peder.frang@gmail.com>
2022-02-03 19:11:43 +0100

    added-user-dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def show
+    if params[:format]
+      @vehicles = Vehicle.where(user_id: params[:format])
+    else
+      @vehicles = Vehicle.where(user_id: current_user.id)
+    end
+    @name = "#{@vehicles.first.user.first_name} #{@vehicles.first.user.last_name}"
+  end
 end
