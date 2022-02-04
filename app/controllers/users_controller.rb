616fac22268e7c9fcce85c3108f693595f68e8ce
peder frang <peder.frang@gmail.com>
2022-02-04 11:08:45 +0100

    fixed-bug-where-i-can-access-my-dashboard-through-index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,13 @@ class UsersController < ApplicationController
       @vehicles = Vehicle.where(user_id: params[:format])
     else
       @vehicles = Vehicle.where(user_id: current_user.id)
-      @out_bookings = Booking.where(user_id: current_user.id)
-      @in_bookings = Booking.where(vehicle_id: @vehicles)
       # @vehicles.each do |vehicle|
       #   @in_bookings << Booking.where(vehicle_id: vehicle.id)
       # end
       # @bookings = Booking.where(vehicle_id: )
     end
+    @out_bookings = Booking.where(user_id: current_user.id)
+    @in_bookings = Booking.where(vehicle_id: @vehicles)
     authorize @vehicles
     @name = "#{@vehicles.first.user.first_name} #{@vehicles.first.user.last_name}"
   end
