b143c934ea411780eecb295aeecf2546bbb1bc19
Florencia Tarocco <florenciatarocco@gmail.com>
2022-02-05 10:59:06 +0100

    add address latitude and longitude to vehicle model

---
+++ db/migrate/20220205095742_add_address_to_vehicle.rb
@@ -0,0 +1,5 @@
+class AddAddressToVehicle < ActiveRecord::Migration[6.1]
+  def change
+    add_column :vehicles, :address, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_201705) do
+ActiveRecord::Schema.define(version: 2022_02_05_095742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,9 @@ ActiveRecord::Schema.define(version: 2022_02_03_201705) do
     t.string "description"
     t.float "price"
     t.string "name"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_vehicles_on_user_id"
   end
 
