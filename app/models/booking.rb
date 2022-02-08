6262214fdec541daf1d6f01be46a7cc96baeaa1b
NIKLAS FRIDMAN <fridman.niklas@gmail.com>
2022-02-08 19:34:05 +0100

    added model, validations, assoications and tested it in console c

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :vehicle
   belongs_to :user
-
+  has_many :reviews
   validates :status, inclusion: { in: ["Accepted", "Pending", "Rejected"] }
   validates :booking_from, :booking_to, presence: true
   # validate :booking_to_after_booking_from
